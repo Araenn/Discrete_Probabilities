e95e84356e7135ed8eaaa20d67f7b4947daeba20
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2021-03-30 10:09:04 +0200

    Update VACU_ab.m

--- VACU_ab.m
+++ VACU_ab.m
@@ -1,4 +1,4 @@
 function Y = VACU_ab(N, a, b)
-    X = rand(N, 1); %tableau de N valeurs a afficher, entre 0 et 1
+    X = rand(N, 1); %tab of N values to show, between 0 and 1
     Y = a + (b - a) .* X;
 end
