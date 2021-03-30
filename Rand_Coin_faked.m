7dd3e4ed46c102540fb6c221019e605c73f609c4
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2021-03-30 09:59:10 +0200

    add comments

--- Rand_Coin_faked.m
+++ Rand_Coin_faked.m
@@ -1,6 +1,6 @@
-function X = Rand_Piece_truquee(N)
+function X = Rand_Coin_faked(N)
   U = rand(N, 1); 
-  p = 0.3;
+  p = 0.3; %probability to get heads or tails (not 0.5/0.5)
   for i = 1:N
     if U(i) <= p
       X(i) = 1;
